2461412efd5f89471b6c96d87ace9f8926ced546
Milamoo12340 <kaigekylo20@gmail.com>
2025-11-19 13:44:00 +0800

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App data-filename="pages/GitHubExport" data-linenumber="89" data-visual-selector-id="pages/GitHubExport89" /></React.StrictMode>);
